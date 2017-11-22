60340529a76a5cc847cbfd3179c56d4638eb99e5
Sam Smith <smithrunner@live.com>
2017-11-22 17:22:09 +0000

    stack and queues complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,23 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue.first
+    @tail = @queue.last
   end
 
   def dequeue
+    first_in = @head
+    @queue.delete(@head)
+    @head = @queue.first
+    return first_in
   end
 
   def empty?
+    if @queue.length > 0
+      return false
+    else
+      return true
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,34 @@ class MyStack
   end
 
   def push(item)
+    push_stack = [item]
+    @stack.each do |element|
+      push_stack << element
+    end
+    @stack = push_stack
+    self.top = @stack.first
   end
 
   def pop
+    pop_stack = []
+    firstelem = ()
+    @stack.each_with_index do |element,index|
+      if index > 0
+        pop_stack << element
+      else
+        firstelem = element
+      end
+    end
+    @stack = pop_stack
+    self.top = @stack.first
+    return firstelem
   end
 
   def empty?
+    if @stack.length > 0 
+      return false
+    else
+      return true
+    end
   end
 end
