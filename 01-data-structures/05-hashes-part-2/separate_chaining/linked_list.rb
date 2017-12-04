9bdd75c58a8f567879e023eb81be14ea56c5a0c2
Sam Smith <smithrunner@live.com>
2017-12-04 16:49:29 +0000

    separate chaining complete

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,79 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if @head == nil
+      @head = node
+      @tail = node
+    elsif @head == @tail
+      @head.next = node
+      @tail = node
+    else
+      @tail.next = node
+      @tail = node
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if @head == @tail
+      @head = nil
+      @tail = nil
+    else
+      index = @head
+      while index.next != @tail
+        index = index.next
+      end
+      @tail = index
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    string = ''
+    index = @head
+    while index != @tail
+      string << index.value.to_s
+      string << "\n"
+      index = index.next
+    end
+    string << @tail.value.to_s
+    string << "\n"
+    puts string
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if node == @head
+      remove_front
+    elsif node == @tail
+      remove_tail
+    else
+      index = @head
+      while index.next != node
+        index = index.next
+      end
+      index.next = node.next
+      node.next = nil
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    node.next = @head
+    @head = node
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    if @head == @tail
+      front = @head
+      @head = nil
+      @tail = nil
+      return front
+    else
+      front = @head
+      @head = @head.next
+      return front
+    end
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,29 +5,83 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
+    @items = Array.new(size)
+    @count = 0
+    display()
   end
 
   def []=(key, value)
+    @count += 1
+    i = index(key,@items.length)
+    node = Node.new(key,value)
+    
+    if @items[i] == nil
+      @items[i] = LinkedList.new
+      @items[i].add_to_tail(node)
+    else
+      @items[i].add_to_tail(node)
+    end
+    
+    if load_factor > @max_load_factor
+      resize()
+    end
+    
+    display()
   end
 
   def [](key)
+    i = index(key,@items.length)
+    if @items[i] == nil
+      nil
+    else
+      check = @items[i].head
+      while check.key != key
+        check = check.next
+      end
+      check.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @count.to_f / size().to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    items_resize = Array.new( size() * 2 )
+    @items.each do |bucket|
+      if bucket != nil
+        i = index(bucket.head.key,items_resize.length)
+        items_resize[i] = bucket
+      end
+    end
+    @items = items_resize
+  end
+  
+  private
+  
+  def display
+    puts "Current Load Factor: #{self.load_factor}"
+    @items.each_with_index do |node, index|
+      if node == nil
+        puts "index #{index}: nil"
+      else
+        puts "index #{index}:"
+        puts "#{node.print}"
+      end
+    end
   end
 end
