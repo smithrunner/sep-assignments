942581f04cc7d7f151bc3b017a81458c220780a7
Sam Smith <smithrunner@live.com>
2017-12-04 21:18:44 +0000

    update spec file for same key, diff value scenario

--- 01-data-structures/04-hashes-part-1/hashclass_spec.rb
+++ 01-data-structures/04-hashes-part-1/hashclass_spec.rb
@@ -49,12 +49,12 @@ RSpec.describe HashClass, type: Class do
       expect(hash.size).to eq 1
     end
 
-    it "resizes the array when a collision occurs and the values do not match" do
+    it "overrides value when keys match" do
       hash = HashClass.new(1)
       hash["key"] = "value"
       expect(hash.size).to eq 1
       hash["key"] = "different"
-      expect(hash.size).to eq 2
+      expect(hash.size).to eq 1
     end
 
     it "sets the value of key to value" do
